d89d07efaae216dde4dba29f6b54cd0513b1039a
Vitali595 <vitaliy.kovalchuk.2001@gmail.com>
2021-04-28 12:30:13 +0300

    8 homework

--- src/p2-homeworks/h5/pages/Junior/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior/Junior.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HW7 from "../../../h7/HW7";
 import s from "./Junior.module.css";
+import HW8 from "../../../h8/HW8";
 
 function Junior() {
     return (
         <div className={s.junior}>
             <HW7/>
+            <HW8/>
         </div>
     )
 }
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,37 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionsType = {type: 'sort', payload: 'up'} | {type: 'sort', payload: 'down'} | {type: 'check', payload: number}
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionsType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            return state
+            let stateCopy = [...state]
+            stateCopy.sort((a, b) => {
+                if (action.payload === "up") {
+                    if (a.name < b.name) {
+                        return -1
+                    } else if (a.name > b.name) {
+                        return 1
+                    } else {
+                        return 0
+                    }
+                } else {
+                    if (a.name > b.name) {
+                        return -1
+                    } else if (a.name < b.name) {
+                        return 1
+                    } else {
+                        return 0
+                    }
+                }
+
+            })
+            return stateCopy
         }
         case 'check': {
             // need to fix
-            return state
+            return state.filter(a => a.age >= action.payload)
         }
         default: return state
     }
